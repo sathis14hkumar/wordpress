a45c743065c199fb6624c1ee09ab0185118c3102
sathis14hkumar <113536898+sathis14hkumar@users.noreply.github.com>
2023-08-07 15:00:34 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'web_dev_test');
+define('DB_NAME', 'example_db');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'example_user');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'example_user');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
