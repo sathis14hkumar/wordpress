e9397e765766d36c0646f34534b040ed4ed747e6
sathis14hkumar <113536898+sathis14hkumar@users.noreply.github.com>
2023-08-07 15:03:03 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'example_db');
 define('DB_USER', 'example_user');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'example_user');
+define('DB_PASSWORD', 'example_pw');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
